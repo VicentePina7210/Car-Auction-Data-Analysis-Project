ef75ff8d00751ab544d7df8faebcb5193c0d9b7a
patrick-b4 <b2705927@ben.edu>
2024-09-16 17:41:53 -0500

    There was an error with the csv file, row 408163 had an extra column, so the row was removed, and the file was updated to fix this issue.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello World")
+import pandas as pd
+import numpy as np
+import random
+import matplotlib.pyplot as plt
 
-print("I now have access to the file")
+car_df = pd.read_csv("car_prices.csv", low_memory=False)
+print(car_df.head(10))
