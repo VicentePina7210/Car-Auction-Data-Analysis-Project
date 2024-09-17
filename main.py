e459173bf2cb94cdf109a9f7a232090303769831
vicentepina7210 <vpina3210@gmail.com>
2024-09-16 22:15:40 -0500

    Added a few lines to clarify and clean the data

--- main.py
+++ main.py
@@ -6,8 +6,22 @@ import matplotlib.pyplot as plt
 car_df = pd.read_csv("car_prices.csv", low_memory=False)
 # First it is important to get an idea of how many null values there are within the dataset
 print(car_df.info())
-# It seems that the last column might be unnecessary as it is unnamed and mostly filled with null values
+
+print(car_df.iloc[:, -1].head(5))
+
+non_null = car_df.iloc[:,-1].dropna()
+
+print(non_null)
+
+#Because the data set is very large we can afford to drop the values that are null
+#first we drop the last column because it only contains 25 non nulls
 car_df = car_df.drop(car_df.columns[-1], axis = 1)
+car_df = car_df.dropna()
+
+print(car_df.info())
+
+#It seems that the last column might be unnecessary as it is unnamed and mostly filled with null values
+
 pd.set_option('display.max_columns', None)
 
 # The saledate column has a format which contains too much information, it is better to convert this to a more standard format.
@@ -20,3 +34,4 @@ car_df['saledate'] = car_df['saledate'].dt.date
 
 
 print(car_df.head(10))
+
