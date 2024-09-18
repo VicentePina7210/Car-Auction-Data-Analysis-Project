c47ec24df81dc201441c02ac59756982d1ebddba
Roy <roywmraziv@icloud.com>
2024-09-17 19:59:50 -0500

    Added another 10 questions for exploration of data

--- main.py
+++ main.py
@@ -103,6 +103,68 @@ print(f"The amount of high mileage cars (miles > 100,000) in the set is: {high_m
 
 #######################End of First 10 Questions###################################
 
+#######################Start of Middle 10 Questions##################################
+
+# What are the most common car makes in the dataset?
+make_counts = car_df['make'].value_counts().head(10) 
+print("The top 10 most common car makes are: ") 
+print(make_counts)
+
+# What is the distribution of car ages? 
+car_df['car_age'] = 2015 - car_df['year']
+car_df['car_age'].hist(bins = 20)
+plt.title('Distribution of Car Ages')
+plt.xlabel('Age (years)')
+plt.ylabel('Frequency')
+plt.show()
+
+# What is the average selling price per car model?
+price_avg_by_model = car_df.groupby('model')['sellingprice'].mean().sort_values(ascending=False)
+print("The average selling price per car model: ")
+print(price_avg_by_model.head(10))
+
+# What is the relationship between car age and selling price?
+car_df_sampled = car_df.sample(5000, random_state= 50)
+car_df_sampled.plot.scatter(x = 'car_age', y = 'sellingprice', title = 'Car Age vs. Selling Price')
+plt.xlabel('Car Age (years)')
+plt.ylabel('Selling Price')
+plt.show()
+
+# Which states have the highest selling price?
+avg_price_by_fuel = car_df.groupby('fuel')['sellingprice'].mean().sort_values(ascending = False)
+print("Average selling price by fuel type: ")
+print(avg_price_by_fuel)
+
+# How does the selling value vary by fuel type?
+avg_price_by_fuel = car_df.groupby('fuel')['sellingprice'].mean().sort_values(ascending = False)
+print("Average selling price by fuel type: ")
+print(avg_price_by_fuel)
+
+# What are the top 5 most common colors?
+color_counts = car_df['paintcolor'].value_counts().head(5)
+print("The top 5 most popular colors are:")
+print(color_counts)
+
+# How does the condition affect price?
+avg_price_by_condition = car_df.groupby('condition')['sellingprice'].mean()
+print('Average selling price by condition:')
+print(avg_price_by_condition)
+
+# How many cars are sold each month?
+car_df['sale_month'] = car_df['saledate'].dt.month
+cars_per_month = car_df['sale_month'].value_counts().sort_index()
+print("Number of cars sold each month:")
+print(cars_per_month)
+
+# What is the average mileage by make?
+car_df['sellingprice'].hist(bins = 50)
+plt.title('Distribution of Selling Prices')
+plt.xlabel('Selling Price')
+plt.ylabel('Frequency')
+plt.show()
+
+#######################End of First 10 Questions###################################
+
 #convert the column to a list
 car_color = car_df['color'].to_list()
 
